05b9f2a0671f6b8847b6415ed31ba290e519886a
任维超 <renweichao@yongche.com>
2016-12-14 15:20:41 +0800

    Initial Commit

---
+++ LanshanTransportationOA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LanshanTransportationOA
+//
+//  Created by 任维超 on 2016/12/14.
+//  Copyright © 2016年 任维超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LanshanTransportationOA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LanshanTransportationOA
+//
+//  Created by 任维超 on 2016/12/14.
+//  Copyright © 2016年 任维超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
